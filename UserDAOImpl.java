2d58aa7281c66a5029881f32d39600a5fc0fde88
Nguyễn Minh Đức <114336100+nguyenminhduc0233@users.noreply.github.com>
2024-12-29 20:57:47 +0700

    Update UserDAOImpl.java

--- UserDAOImpl.java
+++ UserDAOImpl.java
@@ -3,17 +3,24 @@ package com.example.dao;
 import com.example.model.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public class UserDAOImpl implements UserDAO {
+
     @Autowired
     private SessionFactory sessionFactory;
 
-    public User findUserByUsernameAndPassword(String username, String password) {
+    @Override
+    public User getUserByUsername(String username) {
         Session session = sessionFactory.getCurrentSession();
-        return (User) session.createQuery("FROM User WHERE username = :username AND password = :password")
-                             .setParameter("username", username)
-                             .setParameter("password", password)
-                             .uniqueResult();
+        Query<User> query = session.createQuery("FROM User WHERE username = :username", User.class);
+        query.setParameter("username", username);
+        List<User> users = query.getResultList();
+        return users.isEmpty() ? null : users.get(0);
     }
 }
